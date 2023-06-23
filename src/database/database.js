e8952a8fb9d905cf4fbaccbd164f798336ea4434
mirzachairat <mirzachairat5@gmail.com>
2023-06-23 22:41:54 +0700

    setup DB dua

--- src/database/database.js
+++ src/database/database.js
@@ -1,72 +1,27 @@
-// import Sequelize from "sequelize";
+import Sequelize from "sequelize";
+const {
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    DB_URI,
+    DB_PORT
+  } = process.env 
 
-// export const sequelize = new Sequelize(
-//   "postgres", // db name,
-//   "postgres", // username
-//   "bismillah123", // password
-//   {
-//     hostaddr: "34.172.198.10",
-//     dialect: "postgres",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       require: 30000,
-//       idle: 10000,
-//     },
-//     logging: false,
-//   }
-// );
+export const sequelize = new Sequelize(
+  DB_NAME, // db name,
+  DB_USER, // username
+  DB_PASSWORD, // password
+  {
+    hostaddr: DB_URI,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      require: 30000,
+      idle: 10000,
+    },
+    logging: false,
+  }
+);
 
-// export const sequelize = new Sequelize(
-//   "postgres", // db name,
-//   "postgres", // username
-//   "bismillah123", // password
-//   {
-//     dialect: 'postgres',
-//     host: '/cloudsql/${veteriner-382105:us-central1:dbveteriner}',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     },
-//     dialectOptions: {
-//         // e.g. socketPath: '/cloudsql/my-awesome-project:us-central1:my-cloud-sql-instance'
-//         // same as host string above
-//         socketPath: '/cloudsql/${veteriner-382105:us-central1:dbveteriner}'
-//     },
-//     logging: false,
-//     operatorsAliases: false
-//   }
-// );
-
-
-  // import Sequelize from "sequelize";
-  // import dotenv from 'dotenv';
-  // dotenv.config();
-
-  // const {
-  //   DB_NAME,
-  //   DB_PASSWORD,
-  //   DB_USER,
-  //   DB_URI,
-  //   DB_PORT
-  // } = process.env 
   
-  // export const sequelize = new Sequelize(DB_USER, DB_NAME, DB_PASSWORD, {
-  //   DB_URI,
-  //   DB_PORT
-  //   });
-  
-
-// In db.js
-const { Pool } = require("pg");
-
-// The secret connection string you copied earlier
-const connectionString ="PGPASSWORD=Nczq8oB9f3cAUgkdfzXw psql -h containers-us-west-195.railway.app -U postgres -p 6277 -d railway";
-
-const pool = new Pool({
-  connectionString,
-});
-
-module.exports = pool;
